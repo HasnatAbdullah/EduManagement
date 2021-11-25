8b732b447116c3ca48c76fe233af9cca63135d5c
Hasnat Abdullah <45420800+HasnatAbdullah@users.noreply.github.com>
2021-11-25 16:55:54 +0600

    Added files

---
+++ src/client/util/ClientConfig.java
@@ -0,0 +1,8 @@
+package client.util;
+
+public class ClientConfig {
+
+    public static String host = "192.168.100.20";
+    public static Integer port = 40462;
+    public static String fileRecivePath = "F:/FileRecive/files/assignments/";
+}
